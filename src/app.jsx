41a5bc544a8916943a8d696e6ee055fcea15fec6
daphneplott <daphneplott@gmail.com>
2025-10-02 09:12:32 -0600

    created jsx pages

---
+++ src/app.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './app.css';
+
+export default function App() {
+  return (
+    <div className="body bg-dark text-light">
+      <header className="container-fluid">
+        
+      </header>
+
+      <main>App components go here</main>
+
+      <footer className="bg-dark text-white-50">
+        <div className="container-fluid">
+          <span className="text-reset">Author Name(s)</span>
+          <a className="text-reset" href="https://github.com/webprogramming260/simon-react">
+            Source
+          </a>
+        </div>
+      </footer>
+    </div>
+  );
+}
